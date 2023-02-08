00e0013701b3e9a377ef6ab073b3b04eecfc0fc8
Ismael <s.g.ismaelcadenas@gmai.com>
2023-02-08 12:51:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Inicio</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action=index.php method="post">
+        <label for="correo">Introduce tu correo:</label><input type="mail" name="correo" placeholder="palomitas@gmail.com" required>
+        <label for="fecha">Escoge una fecha</label><input type="date" name="fecha" value="<?php echo date('Y-m-d')?>" min="<?php echo date('Y-m-d')?>" required>
+        <input type="submit" name="enviar" value="Buscar asientos">
+    </form>
+</body>
+</html>
